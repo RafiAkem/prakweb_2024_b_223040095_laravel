054fbe99fdef49106f21d911d65068d78c01367e
RafiAkem <yc66zio@gmail.com>
2024-11-03 19:13:35 +0700

    Video ke 8 laravel

---
+++ app/Models/Post.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+use Illuminate\Support\Arr;
+class Post
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => '1',
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Rafi',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti sit ipsam eaque totam modi? Expedita consequuntur beatae doloremque asperiores vero recusandae incidunt maiores voluptatem, quasi alias necessitatibus ipsum deleniti pariatur!'
+            ],
+            [
+                'id' => '2',
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Rafi',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore recusandae, accusantium fugiat consectetur quae corrupti, debitis hic nobis vel delectus veniam facilis. Vitae dolore tempore excepturi rerum, nam repudiandae error!'
+            ]
+        ];
+    }
+    public static function find($slug): array
+    {
+        // return Arr::first(static::all(), function ($post) use ($slug) {
+        //     return $post['slug'] == $slug;
+        // });
+        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
+        if (!$post) {
+            abort(404);
+        }
+        return $post;
+    }
+}
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -77,8 +77,8 @@
             <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
           </div>
           <div class="ml-3">
-            <div class="text-base/5 font-medium text-white">Faris</div>
-            <div class="text-sm font-medium text-gray-400">faris@gmail.com</div>
+            <div class="text-base/5 font-medium text-white">Rafi</div>
+            <div class="text-sm font-medium text-gray-400">Rafi@gmail.com</div>
           </div>
 
         </div>
